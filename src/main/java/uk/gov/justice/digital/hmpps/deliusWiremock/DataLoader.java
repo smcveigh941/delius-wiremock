253b5ce4edfe465aa3ff619028a9214910b17058
Stephen McVeigh <smcveigh941@gmail.com>
2021-10-06 17:05:44 +0100

    Hardcode selected prison codes and remove prison api

--- src/main/java/uk/gov/justice/digital/hmpps/deliusWiremock/DataLoader.java
+++ src/main/java/uk/gov/justice/digital/hmpps/deliusWiremock/DataLoader.java
@@ -2,8 +2,10 @@ package uk.gov.justice.digital.hmpps.deliusWiremock;
 
 import com.github.javafaker.Faker;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -11,7 +13,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.digital.hmpps.deliusWiremock.dao.entity.OffenderEntity;
 import uk.gov.justice.digital.hmpps.deliusWiremock.dao.repository.OffenderRepository;
-import uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.PrisonApiClient;
 import uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.PrisonerSearchApiClient;
 import uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.dto.PrisonerDetailsResponse;
 
@@ -19,16 +20,15 @@ import uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.dto.PrisonerDetail
 public class DataLoader implements ApplicationRunner {
 
   private final OffenderRepository offenderRepository;
-  private final PrisonApiClient prisonApiClient;
   private final PrisonerSearchApiClient prisonerSearchApiClient;
+
   @Value("#{'${dataset.size}'}")
   private Integer numberOfOffenders;
 
   @Autowired
-  public DataLoader(OffenderRepository offenderRepository, PrisonApiClient prisonApiClient,
+  public DataLoader(OffenderRepository offenderRepository,
       PrisonerSearchApiClient prisonerSearchApiClient) {
     this.offenderRepository = offenderRepository;
-    this.prisonApiClient = prisonApiClient;
     this.prisonerSearchApiClient = prisonerSearchApiClient;
   }
 
@@ -36,13 +36,17 @@ public class DataLoader implements ApplicationRunner {
     Faker faker = new Faker();
     List<OffenderEntity> offenders = new ArrayList<>();
 
-    List<String> prisonIds = prisonApiClient.getPrisonIds();
-    Collections.shuffle(prisonIds);
+    List<String> prisonCodes = Arrays.asList("BMI", "LEI", "MDI");
 
     List<PrisonerDetailsResponse> prisonerList = new ArrayList<>();
-    for (int i = 0; i < 10; i++) {
-      prisonerList.addAll(prisonerSearchApiClient.getPrisoners(prisonIds.get(i)).getContent());
+
+    for (String prisonCode : prisonCodes) {
+      prisonerList.addAll(prisonerSearchApiClient.getPrisoners(prisonCode).getContent());
     }
+
+    prisonerList = prisonerList.stream()
+        .filter(prisoner -> prisoner.getConditionalReleaseDate() != null)
+        .collect(Collectors.toList());
 
     Collections.shuffle(prisonerList);
 
--- src/main/java/uk/gov/justice/digital/hmpps/deliusWiremock/httpClient/dto/PrisonerDetailsResponse.java
+++ src/main/java/uk/gov/justice/digital/hmpps/deliusWiremock/httpClient/dto/PrisonerDetailsResponse.java
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.dto;
 
+import java.time.LocalDate;
 import lombok.Data;
 
 @Data
 public class PrisonerDetailsResponse {
 
   private String prisonerNumber;
+  private LocalDate conditionalReleaseDate;
 }
