5a3c6ea16562061c9400c20e56cb1edee93fefbf
Stephen McVeigh <smcveigh941@gmail.com>
2022-02-11 20:35:28 +0000

    Add separate user profiles with cases divided up between COMs

--- src/main/java/uk/gov/justice/digital/hmpps/deliusWiremock/config/DataLoader.java
+++ src/main/java/uk/gov/justice/digital/hmpps/deliusWiremock/config/DataLoader.java
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.hmpps.deliusWiremock.config;
 
 import com.github.javafaker.Faker;
+import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,10 +15,13 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.justice.digital.hmpps.deliusWiremock.dao.entity.OffenderEntity;
+import uk.gov.justice.digital.hmpps.deliusWiremock.dao.entity.StaffEntity;
+import uk.gov.justice.digital.hmpps.deliusWiremock.dao.entity.TeamEntity;
 import uk.gov.justice.digital.hmpps.deliusWiremock.dao.repository.OffenderRepository;
-import uk.gov.justice.digital.hmpps.deliusWiremock.dao.repository.StaffRepository;
 import uk.gov.justice.digital.hmpps.deliusWiremock.dao.repository.TeamRepository;
+import uk.gov.justice.digital.hmpps.deliusWiremock.exception.NotFoundException;
 import uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.PrisonerSearchApiClient;
 import uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.dto.PrisonerDetailsResponse;
 
@@ -22,7 +29,6 @@ import uk.gov.justice.digital.hmpps.deliusWiremock.httpClient.dto.PrisonerDetail
 public class DataLoader implements ApplicationRunner {
 
   private final OffenderRepository offenderRepository;
-  private final StaffRepository staffRepository;
   private final TeamRepository teamRepository;
   private final PrisonerSearchApiClient prisonerSearchApiClient;
 
@@ -32,68 +38,65 @@ public class DataLoader implements ApplicationRunner {
   @Value("classpath:dev.txt")
   Resource devCases;
 
-  @Value("classpath:penTest1Cases.txt")
-  Resource penTest1Cases;
-
-  @Value("classpath:penTest2Cases.txt")
-  Resource penTest2Cases;
-
-  @Value("classpath:acCases.txt")
-  Resource acCases;
-
-  @Value("classpath:unallocatedCases.txt")
-  Resource unallocatedCases;
-
   @Autowired
   public DataLoader(
       OffenderRepository offenderRepository,
-      StaffRepository staffRepository,
       TeamRepository teamRepository,
       PrisonerSearchApiClient prisonerSearchApiClient
   ) {
     this.offenderRepository = offenderRepository;
-    this.staffRepository = staffRepository;
     this.teamRepository = teamRepository;
     this.prisonerSearchApiClient = prisonerSearchApiClient;
   }
 
+  @Transactional
   public void run(ApplicationArguments args) throws IOException {
-    List<String> pb = IOUtils.readLines(devCases.getInputStream());
-    addCases(pb, 2000L, "cvl");
+    List<String> cvl = IOUtils.readLines(devCases.getInputStream());
+    addCases(cvl, "cvl");
 
-    List<String> dev = IOUtils.readLines(penTest1Cases.getInputStream());
-    addCases(dev, 3000L, "pt1");
-
-    List<String> pt1 = IOUtils.readLines(penTest2Cases.getInputStream());
-    addCases(pt1, 4000L, "pt2");
-
-    List<String> pt2 = IOUtils.readLines(acCases.getInputStream());
-    addCases(pt2, 5000L, "ac");
-
-    List<String> ac = IOUtils.readLines(privateBetaCases.getInputStream());
-    addCases(ac, 6000L, "pb");
-
-    List<String> unallocated = IOUtils.readLines(unallocatedCases.getInputStream());
-    addCases(unallocated, null, "pb");
+    //Exclude the devs from getting allocated to user research cases
+    List<String> pb = IOUtils.readLines(privateBetaCases.getInputStream());
+    addCases(pb, "pb", List.of(1000L, 2000L));
   }
 
-  private void addCases(List<String> nomisIds, Long staffId, String teamCode) {
+  private void addCases(List<String> nomisIds, String teamCode, List<Long> staffIdsNotToAllocate) {
     Faker faker = new Faker();
     List<OffenderEntity> offenders = new ArrayList<>();
     List<PrisonerDetailsResponse> prisonerList = prisonerSearchApiClient.getPrisoners(nomisIds);
-    for (PrisonerDetailsResponse prisoner : prisonerList) {
-      OffenderEntity offender = new OffenderEntity();
 
-      offender.setNomsNumber(prisoner.getPrisonerNumber());
-      offender.setCrnNumber(faker.regexify("[A-Z][0-9]{6}"));
-      offender.setCroNumber(faker.regexify("[0-9]{1}/[0-9]{5}"));
-      offender.setPncNumber(faker.regexify("[0-9]{4}/[0-9]{5}"));
-      offender.setStaff(this.staffRepository.findByStaffIdentifier(staffId).orElse(null));
-      offender.setTeam(this.teamRepository.findByTeamCode(teamCode).orElse(null));
+    TeamEntity team = this.teamRepository.findByTeamCode(teamCode)
+        .orElseThrow(NotFoundException::new);
 
-      offenders.add(offender);
+    List<StaffEntity> staffInTeam = team
+        .getStaff()
+        .stream()
+        .filter(staffEntity -> !staffIdsNotToAllocate.contains(staffEntity.getStaffIdentifier()))
+        .sorted(Comparator.comparing(StaffEntity::getStaffIdentifier))
+        .collect(Collectors.toList());
+
+    List<List<PrisonerDetailsResponse>> offendersSharedAmongTeamMembers = Lists.partition(
+        prisonerList, prisonerList.size() / staffInTeam.size());
+
+    for (int i = 0; i < offendersSharedAmongTeamMembers.size(); i++) {
+      List<PrisonerDetailsResponse> shareOfCases = offendersSharedAmongTeamMembers.get(i);
+      for (PrisonerDetailsResponse prisoner : shareOfCases) {
+        OffenderEntity offenderEntity = new OffenderEntity();
+
+        offenderEntity.setNomsNumber(prisoner.getPrisonerNumber());
+        offenderEntity.setCrnNumber(faker.regexify("[A-Z][0-9]{6}"));
+        offenderEntity.setCroNumber(faker.regexify("[0-9]{1}/[0-9]{5}"));
+        offenderEntity.setPncNumber(faker.regexify("[0-9]{4}/[0-9]{5}"));
+        offenderEntity.setStaff(i < staffInTeam.size() ? staffInTeam.get(i) : null);
+        offenderEntity.setTeam(team);
+
+        offenders.add(offenderEntity);
+      }
     }
 
     offenderRepository.saveAllAndFlush(offenders);
+  }
+
+  private void addCases(List<String> nomisIds, String teamCode) {
+    addCases(nomisIds, teamCode, Collections.emptyList());
   }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 -- TEAMS
 INSERT INTO team(id, team_code, team_description, team_telephone, ldu_code, ldu_description, borough_code, borough_description) VALUES
+<<<<<<< Updated upstream
 (1, 'cvl', 'Licence Team', '0800001066', 'N55UNA', 'Nottingham City District', 'N55U', 'Nottingham'),
 (2, 'pt1', 'Pen Test 1', '0800001066', 'N55UNB', 'Nottingham City District', 'N55U', 'Nottingham'),
 (3, 'pt2', 'Pen Test 2', '0800001066', 'N55UNC', 'Nottingham City District', 'N55U', 'Nottingham'),
@@ -13,15 +14,34 @@ INSERT INTO staff(id, staff_identifier, username, staff_code, email, telephone_n
 (3, 4000, 'pt_com2', 'X12348', 'pentester2@probation.gov.uk', '07786 989777', 'Tester', 'Pen2', 'N55', 'Yorkshire and Humber'),
 (4, 5000, 'ac_com', 'X12349', 'accessibility@probation.gov.uk', '07786 989777', '1', 'Accessibility tester', 'N55', 'Yorkshire and Humber'),
 (5, 6000, 'cvl_beta_dev', 'X12345', 'betatester1@probation.gov.uk', '07786 989777','John', 'Smith', 'N55', 'Yorkshire and Humber');
+=======
+(1, 'cvl', 'Licence Team', '0800001066', 'N55UNA', 'Nottingham City District', 'N55', 'Nottingham'),
+(2, 'pb', 'Private Beta Testers', '0800001066', 'N55UNE', 'Nottingham City District', 'N55', 'Nottingham');
+
+-- STAFF
+-- The below contact details are not real
+INSERT INTO staff(id, staff_identifier, username, staff_code, email, telephone_number, staff_forenames, staff_surname) VALUES
+(1, 1000, 'smcveigh2', 'X12340', 'smcveigh2@probation.gov.uk', '07786 989777', 'Stephen', 'McVeigh'),
+(2, 2000, 'timharrison', 'X12341', 'timharrison@probation.gov.uk', '07786 989777', 'Tim', 'Harrison'),
+(3, 3000, 'cvl_com', 'X12342', 'cvl_com@probation.gov.uk', '07786 989777', 'CVL', 'COM'),
+(4, 4000, 'kamsandhu', 'X12343', 'kamsandhu@probation.gov.uk', '07786 989777', 'Kam', 'Sandhu'),
+(5, 5000, 'santhosh', 'X12344', 'santhosh@probation.gov.uk', '07786 989777', 'Santhosh', 'Chinnathambi'),
+(6, 6000, 'jeddyshaw', 'X12345', 'jeddyshaw@probation.gov.uk', '07786 989777', 'Jonathan', 'Eddyshaw'),
+(7, 7000, 'AdamRichardson', 'X12346', 'adamrichardson@probation.gov.uk', '07786 989777', 'Adam', 'Richardson'),
+(8, 8000, 'Laura.Jara.Duncan', 'X12347', 'laura.jara.duncan@probation.gov.uk', '07786 989777', 'Laura', 'Jara Duncan'),
+(9, 9000, 'cvl_beta_dev', 'X12345', 'betatester1@probation.gov.uk', '07786 989777','John', 'Smith');
+>>>>>>> Stashed changes
 
 -- TEAM-STAFF MAP
-INSERT INTO staff_team(team_id, staff_id) VALUES
+INSERT INTO staff_team(staff_id, team_id) VALUES
 (1, 1),
+(1, 2),
 (2, 1),
 (2, 2),
 (3, 1),
-(3, 3),
 (4, 1),
-(4, 4),
 (5, 1),
-(5, 5);
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 2);
